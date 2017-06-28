4c2c07ba5197f5089432cedfee60c9bd5c07c91f
Singularmotor <mom123kkc361@163.com>
2017-06-28 11:39:57 +0800

    charge

--- myscrapy_demo/myscrapy_demo/spiders/demosp.py
+++ myscrapy_demo/myscrapy_demo/spiders/demosp.py
@@ -4,10 +4,9 @@ from scrapy.spider import Spider
 
 class demosp(Spider):
     name="demosp"
-    allowed_domains=["runoob.com"]
+    allowed_domains=["jb51.net"]
     #allow_domains是搜索的域名范围，也就是爬虫的约束区域，规定爬虫只爬取这个域名下的网页。
-    start_urls=["http://www.runoob.com/git/git-tutorial.html",
-                "http://www.runoob.com/python3/python3-tutorial.html"]
+    start_urls=["http://www.jb51.net/article/92516.htm"]
 
     def parse(self,response):
         filename=response.url.split("/")[-2]
